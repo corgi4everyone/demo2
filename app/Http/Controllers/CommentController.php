9ae1b4c64c38064e515ddc56a8ca65913bde765e
theikdisann <2001102theikdi@gmail.com>
2024-11-18 01:13:58 +0700

    create post, comment and quiz controller

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Events\CommentCreated;
+use App\Models\Comment;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class CommentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request, Post $post)
+    {
+        //
+        $request->validate([
+            'content' => 'required',
+        ]);
+        $comment = Comment::create([
+            'post_id' => $post->id,
+            'user_id' => auth()->id,
+            'content' => $request->content
+        ]);
+
+        return redirect(route('post.show', $post));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Comment $comment)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Comment $comment)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Comment $comment)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Comment $comment)
+    {
+        //
+    }
+}
